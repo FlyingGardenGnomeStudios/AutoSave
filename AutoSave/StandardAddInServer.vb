f873448b2b0c80f3984fe5c069b3cfa42a6a57b7
flyinggardengromestudios <flyinggardengnomestudios@gmail.com>
2018-12-06 12:00:16 -0600

    Removed traces of Standalone from Subscription

--- AutoSave/StandardAddInServer.vb
+++ AutoSave/StandardAddInServer.vb
@@ -40,19 +40,19 @@ Namespace AutoSave
         Public Sub Activate(ByVal addInSiteObject As Inventor.ApplicationAddInSite, ByVal firstTime As Boolean) Implements Inventor.ApplicationAddInServer.Activate
             ' Initialize AddIn members.
 
-            If IsFile(IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonApplicationData), "Autodesk\ApplicationPlugins"), "AutoSave.dll") = True Then
+            If IsFile(IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonApplicationData), "Autodesk\ApplicationPlugins"), "AutoSave-Standalone.dll") = True Then
                 'If IO.File.Exists(IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonApplicationData), "Autodesk\ApplicationPlugins\FlyingGarden_AutoSave.bundle\Contents\AutoSave.dll")) Then
-                MsgBox("It appears as though the subscription version of Autosave is installed" & vbNewLine &
+                MsgBox("It appears as though the perpetual version of Autosave is installed" & vbNewLine &
                        "In order to stop save conflicts, please uninstal one of the AutoSave versions." & vbNewLine &
-                       "The standalone version will not be loaded.")
+                       "The perpetual version will not be loaded.")
                 Exit Sub
             End If
             g_inventorApplication = addInSiteObject.Application
             ' Connect to the user-interface events to handle a ribbon reset.
             m_uiEvents = g_inventorApplication.UserInterfaceManager.UserInterfaceEvents
             m_UIEvents2 = g_inventorApplication.CommandManager.UserInputEvents
-            Dim largeIcon As stdole.IPictureDisp = PictureDispConverter.ToIPictureDisp(My.Resources.AutoSave_Icon_Perm_32)
-            Dim smallIcon As stdole.IPictureDisp = PictureDispConverter.ToIPictureDisp(My.Resources.AutoSave_Icon_Perm_16)
+            Dim largeIcon As stdole.IPictureDisp = PictureDispConverter.ToIPictureDisp(My.Resources.AutoSave_32)
+            Dim smallIcon As stdole.IPictureDisp = PictureDispConverter.ToIPictureDisp(My.Resources.AutoSave_16)
             Dim controlDefs As Inventor.ControlDefinitions = g_inventorApplication.CommandManager.ControlDefinitions
             ' ActivationCheck()
 
